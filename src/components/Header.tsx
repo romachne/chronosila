11f55bfdf0ce6ebb80a48aecdaf207a095f3f8e4
romachne <romachne@gmail.com>
2021-01-12 03:15:45 +0500

    horizontal chart

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <header>
+      <h1>chronosila</h1>
+      <hr />
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/MainPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Timeline from "./Timeline";
+
+class MainPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Timeline />
+      </div>
+    )
+  }
+}
+export default MainPage
